c7e7fbd52ed6f036d0e76c475a7cc99fd6d3e0ad
thirstyice <tauranwood@gmail.com>
2017-09-22 01:07:10 -0600

    Updated z-probe offset

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -665,7 +665,7 @@
  */
 #define X_PROBE_OFFSET_FROM_EXTRUDER -34  // X offset: -left  +right  [of the nozzle]
 #define Y_PROBE_OFFSET_FROM_EXTRUDER -25  // Y offset: -front +behind [the nozzle]
-#define Z_PROBE_OFFSET_FROM_EXTRUDER -1.37   // Z offset: -below +above  [the nozzle]
+#define Z_PROBE_OFFSET_FROM_EXTRUDER -0.55   // Z offset: -below +above  [the nozzle]
 
 // X and Y axis travel speed (mm/m) between probes
 #define XY_PROBE_SPEED 8000
