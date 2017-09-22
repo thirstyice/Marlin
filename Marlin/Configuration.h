eed75fce56a116b6ffacff23926eae4ae93e5a16
thirstyice <tauranwood@gmail.com>
2017-09-22 01:14:56 -0600

    Updated bed size

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -760,7 +760,7 @@
 #define Y_MIN_POS 0
 #define Z_MIN_POS 0
 #define X_MAX_POS 200
-#define Y_MAX_POS 180
+#define Y_MAX_POS 190
 #define Z_MAX_POS 190
 
 // If enabled, axes won't move below MIN_POS in response to movement commands.
