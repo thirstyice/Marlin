d455cca23699d91f06526e1e9f0732fbe893407d
thirstyice <thirstyice@users.noreply.github.com>
2017-12-10 08:08:56 -0700

    Reduced max z feedrate

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -532,7 +532,7 @@
  * Override with M203
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_MAX_FEEDRATE          { 300, 300, 40, 25 }
+#define DEFAULT_MAX_FEEDRATE          { 300, 300, 30, 25 }
 
 /**
  * Default Max Acceleration (change/s) change = mm/s
