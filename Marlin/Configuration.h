683d18f81d7bfe8464b39d9effc723246d530c19
thirstyice <tauranwood@gmail.com>
2017-12-10 07:02:55 -0700

    Expanded z limit

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -784,7 +784,7 @@
 
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
-#define Z_MAX_POS 190
+#define Z_MAX_POS 210
 
 // If enabled, axes won't move below MIN_POS in response to movement commands.
 #define MIN_SOFTWARE_ENDSTOPS
